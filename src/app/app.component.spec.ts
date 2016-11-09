aa0509e0f5682544b82b5271ed3096343c5b98bf
Jason Kulatunga <jk17@ualberta.ca>
2016-11-09 09:38:44 -0800

    update to ruelette.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: Wanderer', () => {
+describe('App: Ruelette', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { AgmCoreModule } from 'angular2-google-maps/core';
-
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import { AgmCoreModule } from 'angular2-google-maps/core';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterializeModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB694dIrwnp4s9MrjrUi6w4_BEUvxh3FkU'
     })
